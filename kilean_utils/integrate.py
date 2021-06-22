576cfd437e4a552cf4146bd69cdf70c44a239df4
kileean20 <kilean@lbl.gov>
2021-06-21 18:20:41 -0700

    tabularize scipy legendre

--- kilean_utils/integrate.py
+++ kilean_utils/integrate.py
@@ -1,5 +1,14 @@
 from scipy.special import roots_legendre
 import numpy as np
+
+_ORDERS = [16,32,64,128]
+_X0 = {}
+_W0 = {}
+
+for order in _ORDERS:
+  _x0,_w0 = roots_legendre(_ORDERS)
+  _X0[order]=_x0
+  _W0[order]=_w0
   
 
 def gaussian_quad(func,a,b,n_sub_interval=8,n_order=32):
@@ -8,12 +17,16 @@ def gaussian_quad(func,a,b,n_sub_interval=8,n_order=32):
   '''
   L = b-a
   dL = L/n_sub_interval
-  X0,W0 = roots_legendre(n_order)
-  X = dL*(X0+1.)/2. 
+  if n_order in _ORDERS:
+    x0 = _X0[n_order]
+    w0 = _W0[n_order]
+  else:
+    x0,w0 = roots_legendre(n_order)
+  X = dL*(x0+1.)/2. 
   y = 0.
   for i in range(n_sub_interval):
     x_min = a + i*dL
-    y = y + np.sum(W0*func(X+x_min), axis=-1)
+    y = y + np.sum(w0*func(X+x_min), axis=-1)
   return y*dL/2.0
 
 
@@ -27,15 +40,19 @@ def gaussian_quad2D(func,xmin,xmax,ymin,ymax,n_sub_interval_x=8, n_sub_interval_
   Ly = ymax-ymin
   dLx = Lx/n_sub_interval_x
   dLy = Ly/n_sub_interval_y
-  X0,W0 = roots_legendre(n_order)
-  x = dLx*(X0+1.)/2. 
-  y = dLy*(X0+1.)/2. 
+  if n_order in _ORDERS:
+    x0 = _X0[n_order]
+    w0 = _W0[n_order]
+  else:
+    x0,w0 = roots_legendre(n_order)
+  x = dLx*(x0+1.)/2. 
+  y = dLy*(x0+1.)/2. 
   z = 0.
   for i in range(n_sub_interval_x):
     x_min = xmin + i*dLx
     tmp = 0
     for j in range(n_sub_interval_y):
         y_min = ymin + j*dLy
-        tmp += np.sum(W0*func(x+x_min,y+y_min), axis=-1)
-    z += np.sum(W0*tmp, axis=-1)
+        tmp += np.sum(w0*func(x+x_min,y+y_min), axis=-1)
+    z += np.sum(w0*tmp, axis=-1)
   return z*dLx*dLy/4.0
